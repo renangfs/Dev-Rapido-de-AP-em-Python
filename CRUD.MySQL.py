14886589d15c9e918c58d4ceea94a1919dd836b6
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-06-02 10:02:28 -0300

    Update CRUD.MySQL.py

--- CRUD.MySQL.py
+++ CRUD.MySQL.py
@@ -13,8 +13,21 @@ app.resizable(False, False)#usuario não pode alterar o tamanho da janela
 def mudar(altid,anome,an1,an2):
     fr_quadro2=Frame(app, borderwidth=1,relief="solid",background="#dde")
     fr_quadro2.place(x=0,y=0,width=1020,height=720)
-    media=101020
     try:
+        print(an1)
+        print(type(an1))#vendo o tipo primitivo de vn1
+        an1=float(an1)#tranformando vn1 para int
+        print(type(an1))#confirmando se vn1 é int
+
+        print(an2)
+        print(type(an2))#vendo o tipo primitivo de vn2
+        an2=float(an2)#tranformando vn2 para int
+        print(type(an2))#confirmando se vn2 é int
+
+        media=(an1+an2)/2
+        print(media)
+        print(type(media))
+   
         alterar_dados = "UPDATE aluno SET nome = '{}', nota1 = '{}', nota2 = '{}', media = '{}' WHERE matricula = '{}' ".format(anome,an1,an2,media,altid)
 
         cursor = conn.cursor()
@@ -25,46 +38,59 @@ def mudar(altid,anome,an1,an2):
         msgE1=Label(app, text="Registros alterado com sucesso!\n\nLinhas afetadas:",bg="#dde",fg="green",font="consolas 40",anchor=W).place(x=50,y=250,width=900,height=220)#msg ao cliente
         msgE3=Label(app, text=cursor.rowcount,bg="#dde",fg="green",font="consolas 40",anchor=W).place(x=750,y=390,width=300,height=60)# msg ao cliente 
         voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=450,y=470,width=130,height=50)# Cria o botão e envia para função Gravar dados
-        voltar.pack() 
-    except Error as erro:
+        
+    except ValueError as erro:
         msgE2=Label(app, text="Falha ao atualizar os dados",bg="#dde",fg="red",font="consolas 30",anchor=W).place(x=50,y=250,width=900,height=60)# msg ao cliente
         msgE3=Label(app, text=erro,bg="#dde",fg="red",font="consolas 20",anchor=W).place(x=50,y=300,width=900,height=60)# msg ao cliente
+        voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=400,y=500,width=130,height=50)# Cria o botão e envia para função Gravar dados
         print(erro)
         MENU=(input("Pressione qualquer tecla para voltar ao menu...."))
-        voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=400,y=500,width=130,height=50)# Cria o botão e envia para função Gravar dados
-        voltar.pack() 
+        
+        
     
 def GravaDados(vnome,vmatri,vn1,vn2):#função para gravar dados
     fr_quadro2=Frame(app, borderwidth=1,relief="solid",background="#dde")#fundo padrão para sobrepor funções anteriores
     fr_quadro2.place(x=0,y=0,width=1020,height=720)
-    media="1010"
-    #media=vn1+vn2/2 ............problema ao calcular media
+
     try:#Tratamento de erro
-        dados = vmatri+',\''+vnome+'\',\''+vn1+'\',\''+vn2+'\',\''+media+'\')'#padrões (\'') para por aspa no inicio ('\')para por aspa no final
-        #print (dados) ...ver as ordens dos dados
-        comando="""INSERT INTO aluno (matricula,nome,nota1,nota2,media) VALUES("""           
-        sql = comando+dados   
-            
+        print(vn1)
+        print(type(vn1))#vendo o tipo primitivo de vn1
+        vn1=float(vn1)#tranformando vn1 para int
+        print(type(vn1))#confirmando se vn1 é int
+
+        print(vn2)
+        print(type(vn2))#vendo o tipo primitivo de vn2
+        vn2=float(vn2)#tranformando vn2 para int
+        print(type(vn2))#confirmando se vn2 é int
+
+        media=(vn1+vn2)/2
+        print(media)
+        print(type(media))
+
+        sql ="INSERT INTO aluno (matricula,nome,nota1,nota2,media) VALUES('{}','{}','{}','{}','{}')".format(vmatri,vnome,vn1,vn2,media)
         cursor = conn.cursor()
         cursor.execute(sql)
-        conn.commit()#Salvando alterações
+        conn.commit()#Salvando alterações 
+    except ValueError as erro:# tratamento de erro caso nenhum valor nos campos
+        msgE2=Label(app, text="Falha ao salvar os dados",bg="#dde",fg="red",font="consolas 30",anchor=W).place(x=50,y=250,width=900,height=60)# msg ao cliente
+        msgE3=Label(app, text=erro,bg="#dde",fg="red",font="consolas 20",anchor=W).place(x=50,y=300,width=900,height=60)# msg ao cliente
+        voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=450,y=400,width=130,height=50)#Voltar
+    except mysql.connector.errors.IntegrityError as erro:#ratamento de erro caso chave primaria iguais
+        msgE2=Label(app, text="Falha ao salvar os dados",bg="#dde",fg="red",font="consolas 30",anchor=W).place(x=50,y=250,width=900,height=60)# msg ao cliente
+        msgE3=Label(app, text=erro,bg="#dde",fg="red",font="consolas 20",anchor=W).place(x=50,y=300,width=900,height=60)# msg ao cliente
+        voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=450,y=400,width=130,height=50)#Voltar
+    else:
         msgE1=Label(app, text="Arquivo Cadastrado com sucesso!",bg="#dde",fg="green",font="consolas 40",anchor=W).place(x=50,y=250,width=900,height=60)#msg ao cliente
         print("\n***Registro INSERIDO com sucesso!!!***\n")
         print(cursor.rowcount,"Registros na tabela")#mostra a quantidade de registros na tabela
-        voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=450,y=400,width=130,height=50)# Cria o botão e envia para função Gravar dados
-        voltar.pack() 
-    except Error as erro:
-        msgE2=Label(app, text="Falha ao salvar os dados",bg="#dde",fg="red",font="consolas 30",anchor=W).place(x=50,y=250,width=900,height=60)# msg ao cliente
-        msgE3=Label(app, text=erro,bg="#dde",fg="red",font="consolas 20",anchor=W).place(x=50,y=300,width=900,height=60)# msg ao cliente
-        voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=450,y=400,width=130,height=50)# Cria o botão e envia para função Gravar dados
-        voltar.pack() 
+        voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=450,y=400,width=130,height=50)#Vol
+        
 
 def registrar():
+    
     fr_quadro2=Frame(app, borderwidth=1,relief="solid",background="#dde")
     fr_quadro2.place(x=0,y=0,width=1020,height=720)
 
-    
-    
     altid=Label(app, text="CADASTRAR DADOS",bg="#dde",fg="black",font="consolas 35 bold",anchor=W).place(x=300,y=20,width=600,height=40)
 
     vnome=Label(app, text="Nome:",bg="#dde",fg="black",font="consolas 15",anchor=W).place(x=300,y=80,width=600,height=40)#Nome
@@ -82,11 +108,10 @@ def registrar():
     vn2=Label(app, text="Nota 2:",bg="#dde",fg="black",font="consolas 15",anchor=W).place(x=300,y=400,width=600,height=40)#Nota 2
     vn2=Entry(app, textvariable=vn2,font="times 20")
     vn2.place(x=300,y=440,width=400,height=40)
+        
+    Envia = Button(app, text="Enviar",bg="#808080",fg="#010101", command=lambda:GravaDados(vnome.get(),vmatri.get(),vn1.get(),vn2.get()),font="consolas 25 bold").place(x=390,y=500,width=200,height=50)# Cria o botão e envia para função Gravar dados
     
 
-    Envia = Button(app, text="Enviar",bg="#808080",fg="#010101", command=lambda:GravaDados(vnome.get(),vmatri.get(),vn1.get(),vn2.get()),font="consolas 25 bold").place(x=390,y=500,width=200,height=50)# Cria o botão e envia para função Gravar dados
-    Envia.pack() 
-    
 
 def consultar():
     conn = mysql.connector.connect(host='localhost',database='crudtkinter',user='root',password='')# (conn)variavel global para conexão do banco de dados
@@ -106,11 +131,7 @@ def consultar():
         txt=Text(app)
         txt.config(font="consolas 15 bold")
         txt.place(x=10,y=100,width="1000",height="600")
-        a="  "
-        '''b=[1]
-        c=[2]
-        d=[3]
-        e=[4]'''
+
         colunas = 0
         for colunas in linhas: #cria uma estrutura de repatição que é apontada atraves dos vetores
                  cp1 ='Matricula:',colunas[0],'Nome:',colunas[1],'nota1:',colunas[2],'nota2:',colunas[3],'media:',colunas[4]
@@ -118,17 +139,17 @@ def consultar():
                  separa="\n------------------------------------------------------------------------------------------"
                  txt.insert(0.0,separa)
         if linhas == linhas:
-                voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=10,y=20,width=130,height=50)# Cria o botão e envia para função Gravar dados
-                voltar.pack() 
+                 voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=10,y=20,width=130,height=50)#Voltar
                    
     except Error as erro:#Tratamento de erro
-        print("Erro ao acessar o SGBD",erro)
+         print("Erro ao acessar o SGBD",erro)
     finally:#fechando conexão
         if conn.is_connected():
             cursor.close()
             conn.close()
             print("***Conexão Encerrada***") 
             MENU=(input("Pressione qualquer tecla para voltar ao menu...."))
+           
 
 def alterar():
     fr_quadro2=Frame(app, borderwidth=1,relief="solid",background="#dde")
@@ -154,7 +175,7 @@ def alterar():
     an2.place(x=300,y=440,width=400,height=40)
 
     Envia = Button(app, text="Enviar",bg="#808080",fg="#010101", command=lambda:mudar(altid.get(),anome.get(),an1.get(),an2.get()),font="consolas 25 bold").place(x=380,y=500,width=200,height=50)# Cria o botão e envia para função Gravar dados
-    Envia.pack() 
+
 
 def delldados(pesqId):
     fr_quadro2=Frame(app, borderwidth=1,relief="solid",background="#dde")
@@ -169,13 +190,13 @@ def delldados(pesqId):
         msgE1=Label(app, text="Registro EXCLUIDO com sucesso!\n\nLinhas afetadas:",bg="#dde",fg="green",font="consolas 40",anchor=W).place(x=50,y=250,width=900,height=220)#msg ao cliente
         msgE3=Label(app, text=cursor.rowcount,bg="#dde",fg="green",font="consolas 40",anchor=W).place(x=750,y=390,width=300,height=60)# msg ao cliente 
         voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=450,y=470,width=130,height=50)# Cria o botão e envia para função Gravar dados
-        voltar.pack() 
+        
     except Error as erro:
         print("ok")
         msgE2=Label(app, text="Falha ao excluir os dados",bg="#dde",fg="red",font="consolas 30",anchor=W).place(x=50,y=250,width=900,height=60)# msg ao cliente
         msgE3=Label(app, text=erro,bg="#dde",fg="red",font="consolas 18",anchor=W).place(x=50,y=300,width=900,height=60)# msg ao cliente
         voltar = Button(app, text="voltar",bg="#808080",fg="#010101",font="consolas 25 bold",command=lambda: menu()).place(x=450,y=400,width=130,height=50)# Cria o botão e envia para função Gravar dados
-        voltar.pack() 
+        
 
 def excluir():
     fr_quadro2=Frame(app, borderwidth=1,relief="solid",background="#dde")
@@ -186,7 +207,7 @@ def excluir():
     pesqId.place(x=300,y=220,width=400,height=40)
 
     Envia = Button(app, text="Enviar",bg="#808080",fg="#010101", command=lambda:delldados(pesqId.get()),font="consolas 25 bold").place(x=380,y=400,width=200,height=50)# Cria o botão e envia para função Gravar dados
-    Envia.pack()
+    
 
         
 
